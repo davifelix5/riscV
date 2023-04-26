dbd07c6bedf900a5cee92bcc39fcc0238ea628f3
Davi Felix <davifelix@usp.br>
2023-04-26 02:13:15 -0300

    update decoder script

--- utils/decoder.py
+++ utils/decoder.py
@@ -1,8 +1,8 @@
-for i in range(1, 32):
-  binario = bin(i)[2:].zfill(5)
-  complementos = ["~" if binario[i] == "0" else "" for i in range(5)]
-  linha = f"assign OUT[{i}] = EN"
-  for j in range(5):
-    complemento = "" if binario[j] == "1" else "~" 
-    linha += f" & {complemento}IN[{4-j}]"
-  print(linha+";")
+def decoder_m_to_n(m, n):
+  for i in range(n):
+    binario = bin(i)[2:].zfill(m)
+    linha = f"assign OUT[{i}] = EN"
+    for j in range(m):
+      complemento = "" if binario[j] == "1" else "~" 
+      linha += f" & {complemento}IN[{m-1-j}]"
+    print(linha+";")
