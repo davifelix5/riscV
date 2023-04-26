8c2c1eb8e3be9bcf85d0190c229e5f35faec7b5a
Davi Felix <davifelix@usp.br>
2023-04-25 23:28:59 -0300

    parametrize memory

--- datapath/datamemory.v
+++ datapath/datamemory.v
@@ -1,15 +1,15 @@
 
 // Memória de dados
-module DATAMEMORY (
-    input wire[4:0] ADDR, // Endereço de memória
+module DATAMEMORY #(parameter SIZE = 64, parameter N = 32) (
+    input wire[$clog2(N) - 1:0] ADDR, // Endereço de memória
     input wire WE, // Write-Enable: caso esteja ativo, valor em D_in é salvo no endereço ADDR
-    input wire[63:0] D_in, // Entrada de dados
-    output wire[63:0] D_out, // Saída de dados
+    input wire[SIZE-1:0] D_in, // Entrada de dados
+    output wire[SIZE-1:0] D_out, // Saída de dados
     input wire CLK
 );
 
 
-    reg[63:0] MEM[31:0];
+    reg[SIZE-1:0] MEM[N - 1:0];
     
     // Inicializando a palavra 16 com um valor arbitrário para teste
     initial begin
