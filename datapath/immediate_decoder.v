8c155de284034a7c39554ef150dcf1ff90d4991f
Davi Felix <davifelix@usp.br>
2023-05-23 23:08:18 -0300

    decode immediate of U-type instruction

--- datapath/immediate_decoder.v
+++ datapath/immediate_decoder.v
@@ -11,6 +11,8 @@ module immediate_decoder(
       7'b1100011: immediate = {{52{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
       // J-type
       7'b1101111: immediate = {{43{instruction[31]}}, instruction[19:12], instruction[12], instruction[20], instruction[30:21], 1'b0};
+      // U-type
+      7'b0010111: immediate = {{32{instruction[31]}}, instruction[31:12], 12'b0};
       // I-type
       default: immediate = instruction[31:20];
     endcase
