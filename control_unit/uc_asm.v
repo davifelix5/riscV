5997718e0fa1bde02f8439abf6f2503870fa6be1
Davi Felix <davifelix@usp.br>
2023-05-30 21:01:57 -0300

    refactor uc_asm

--- control_unit/uc_asm.v
+++ control_unit/uc_asm.v
@@ -14,10 +14,10 @@ module uc_asm (
 );
 
   parameter FETCH = 3'b000,
-            DECODE = 3'b001,
-            EXECUTE_ADDSUB = 3'b010,
-            EXECUTE_ADDI = 3'b011,
-            WRITE_BACK = 3'b100;
+             DECODE = 3'b001,
+             EXECUTE_ADDSUB = 3'b010,
+             EXECUTE_ADDI = 3'b011,
+             WRITE_BACK = 3'b100;
 
   reg[2:0] current_state, next_state;
 
@@ -31,65 +31,74 @@ module uc_asm (
   end
 
   always @(current_state) begin
+	 next_state = 3'bxxx;
     case (current_state)
       FETCH: begin
-        next_state <= DECODE;
+        next_state = DECODE;
       end
 
       DECODE: begin
         if (opcode == 7'b0010011) begin
-          next_state <= EXECUTE_ADDI;
+          next_state = EXECUTE_ADDI;
         end
         else begin
-          next_state <= EXECUTE_ADDSUB;
+          next_state = EXECUTE_ADDSUB;
         end
       end
 
       EXECUTE_ADDSUB: begin
-        next_state <= WRITE_BACK; 
+        next_state = WRITE_BACK; 
       end
 
       EXECUTE_ADDI: begin
-        next_state <= WRITE_BACK; 
+        next_state = WRITE_BACK; 
       end
 
       default: begin
-        next_state <= FETCH;
+        next_state = FETCH;
       end
     endcase
   end
 
-    always @(current_state) begin
-      case (current_state)
-        FETCH: begin
-          // PC = PC + 4
-          // IR = INSTRUCTION_MEMORY[PC]
-          pc_next_sel = 1'b0;
-          load_ir = 1'b1;
-          load_pc = 1'b1;
-          WE_RF = 1'b0;
-          WE_MEM = 1'b1;
-          RF_din_sel = 1'b0;
-          ULA_din2_sel = 1'b0;
-          addr_sel = 1'b1;
-        end 
-        DECODE: begin
-          load_ir = 1'b0;
-          load_pc = 1'b0;
-          addr_sel = 1'b0;
-        end
-        EXECUTE_ADDSUB: begin
-          RF_din_sel = 2'b01;
-          ULA_din2_sel = 1'b0;
-        end
-        EXECUTE_ADDI: begin
-          RF_din_sel = 2'b01;
-          ULA_din2_sel = 1'b1;
-        end
-        WRITE_BACK: begin
-          WE_RF = 1'b1;
-        end
-      endcase
+    always @(posedge clk or posedge reset) begin
+		if (reset) begin
+			pc_next_sel <= 1'b0;
+      load_ir <= 1'b0;
+      load_pc <= 1'b0;
+      WE_RF <= 1'b0;
+      WE_MEM <= 1'b0;
+      RF_din_sel <= 1'b0;
+      ULA_din2_sel <= 1'b0;
+      addr_sel <= 1'b0;
+		end
+		else begin
+			case (next_state)
+			  FETCH: begin
+				 // PC = PC + 4
+				 // IR = INSTRUCTION_MEMORY[PC]
+				 load_ir <= 1'b1;
+				 load_pc <= 1'b1;
+				 addr_sel <= 1'b1;
+			   WE_RF <= 1'b0;
+			  end 
+			  DECODE: begin
+				 load_ir <= 1'b0;
+				 load_pc <= 1'b0;
+				 addr_sel <= 1'b0;
+			  end
+			  EXECUTE_ADDSUB: begin
+				 RF_din_sel <= 2'b01;
+				 ULA_din2_sel <= 1'b0;
+			  end
+			  EXECUTE_ADDI: begin
+				 RF_din_sel <= 2'b01;
+				 ULA_din2_sel <= 1'b1;
+			  end
+			  WRITE_BACK: begin
+				 WE_RF <= 1'b1;
+			  end
+			endcase
+		end
     end
   
 endmodule
